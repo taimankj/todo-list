9d09199eda63b1e204aa8d98a5ebca8622055828
taimankj <taimanglooni@gmail.com>
2026-09-15 15:38:45 -0700

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,10 @@
+import path from "node:path";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+};
