41cbaa94c49f6eec059fc08f77a4b75ccdb88a14
Keoni Taimanglo <taimanglooni@gmail.com>
2026-09-15 20:59:01 -0700

    Project and Task classes created

---
+++ src/scripts/project.js
@@ -0,0 +1,39 @@
+export class Project {
+  constructor(title) {
+    this._tasks = [];
+    this._title = title;
+  }
+
+  get tasks() {
+    return this._tasks;
+  }
+
+  set tasks(tasks) {
+    this._tasks = tasks;
+  }
+
+  addTask(task) {
+    this._tasks.push(task);
+  }
+
+  removeTask(task) {
+    this.tasks = this.tasks.filter(
+      (ele) =>
+        !(
+          ele.title === task.title &&
+          ele.date === task.date &&
+          ele.priority === task.priority &&
+          ele.description === task.description &&
+          ele.project.title === task.project.title
+        ),
+    );
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(title) {
+    this._title = title;
+  }
+}
---
+++ src/scripts/task.js
@@ -0,0 +1,49 @@
+export class Task {
+  constructor(title, date, priority, description, project) {
+    this._title = title;
+    this._date = date;
+    this._priority = priority; // 1 - low, 2 - med, 3 - high
+    this._description = description;
+    this._project = project;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(title) {
+    this._title = title;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  set date(date) {
+    this._date = date;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+
+  set priority(priority) {
+    this._priority = priority;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(description) {
+    this._description = description;
+  }
+
+  get project() {
+    return this._project;
+  }
+
+  set project(project) {
+    this._project = project;
+  }
+}
