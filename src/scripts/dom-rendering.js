d173d87fdfb56d6ffa3174b29631d6efe7c9a910
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-09-19 00:02:11 -0700

    Refactor project rendering behavior
    - dynamically create the project title and new task button
    - extract task rendering into a separate renderTasks function
    - update project pane clearing to remove all project info
    - simplify the template to use empty project containers

--- src/scripts/dom-rendering.js
+++ src/scripts/dom-rendering.js
@@ -9,9 +9,23 @@ import {
 // get project in localStorage
 // render project and associated tasks onto main pane
 function renderProject(project) {
-  const projectTitleContainer = document.querySelector(".project-title");
-  projectTitleContainer.innerText = project.title;
+  const projectInfo = document.querySelector(".project-info");
+  const projectTitle = document.createElement("h1");
+  const newTaskBtn = document.createElement("button");
 
+  projectTitle.className = "project-title";
+  projectTitle.innerText = project.title;
+
+  newTaskBtn.id = "new-task";
+  newTaskBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="currentColor" /></svg>`;
+
+  renderTasks(project);
+
+  projectInfo.appendChild(projectTitle);
+  projectInfo.appendChild(newTaskBtn);
+}
+
+function renderTasks(project) {
   const tasksContainer = document.querySelector(".project-tasks");
   removeChildren(tasksContainer);
 
@@ -44,10 +58,10 @@ function renderProject(project) {
 }
 
 function clearProjectPane() {
-  const projTitle = document.querySelector(".project-title");
+  const projectInfo = document.querySelector(".project-info");
   const projTasks = document.querySelector(".project-tasks");
 
-  removeChildren(projTitle);
+  removeChildren(projectInfo);
   removeChildren(projTasks);
 }
 
